41f9146f17f59a5c7b2019789be26dee3d295b6e
Aizirek Ismailova <aizirek23072002@gmail.com>
2022-02-28 10:16:28 +0300

    добавить фай

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Task</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <img class="header__logo" src="img/polytech.png" alt="logo"/>
+    <h1>Московский Политех</h1>
+</header>
+<main>
+    <?= '<h2>"Hello World!"</h2>' ?>
+</main>
+<footer>
+   <p> Задание для самостоятельной работы</p>
+</footer>
+</body>
+</html>
